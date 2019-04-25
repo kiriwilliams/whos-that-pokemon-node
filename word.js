a4d0de1d52b98f5d2544368e4328750273ad9632
sharky <zeebing@gmail.com>
2019-04-25 17:16:52 -0400

    added a property 'cleared' to track whether the user has guessed the entire word. Updated 'this.update' method to set 'cleared' to true if there are no longer any underscores in the word

--- word.js
+++ word.js
@@ -2,6 +2,7 @@ const Letter = require("./letter.js");
 
 function Word(){
     this.letters = [],
+    this.cleared = false,
     this.load = function(word){
         for (var i = 0; i < word.length; i++){
             this.letters.push(new Letter(word[i]));
@@ -12,13 +13,22 @@ function Word(){
         for (var i = 0; i < this.letters.length; i ++){
             display += this.letters[i].show() + " ";
         }
+        if(display.indexOf("_") === -1){
+            this.cleared = true;
+        }
         console.log(display);
         return display;
     },
     this.check = function(char){
+        var correct = false;
         for (var i = 0; i < this.letters.length; i ++){
-            this.letters[i].check(char);
-        }        
+            var guessCheck = this.letters[i].check(char);
+            if(guessCheck){
+                correct = true;
+            }
+        }
+        return correct;
+               
     };
 }
 
